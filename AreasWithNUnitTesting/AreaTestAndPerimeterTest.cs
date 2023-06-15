91213b0d53a50b5f7088d6ee99d878368c93daf8
VijaiPatel <48962282+VijaiPatel@users.noreply.github.com>
2023-06-15 15:58:29 +0100

    Uninstalled unnecessary NuGet Packages. Note unable to uninstall Gherkin NuGet Package, added comment about this on line 7.

--- AreasWithNUnitTesting/AreaTestAndPerimeterTest.cs
+++ AreasWithNUnitTesting/AreaTestAndPerimeterTest.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
-
+//unable to uninstall gherkin NuGet package
 
 namespace AreasWithNUnitTesting
 {   [TestFixture]
